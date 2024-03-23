15b3dc612ebac5717cfd9ddf259b4a77f3a2955e
Monody22 <justinyeager599@gmail.com>
2024-03-23 11:46:36 +0700

    nambab bab 1

--- New Text Document.php
+++ New Text Document.php
@@ -1,24 +1,32 @@
 <?php
-// Mendefinisikan array pertama
-$nama = array("Yuna", "Karina", "Yeji", "Wendy", "Winter");
 
-// Mendefinisikan array kedua
-$matkul = array("DKP", "Fisdas", "Kalkulus", "Aljabar Linear", "Matematika Diskrit");
-
-// Fungsi untuk menampilkan array
-function tampilkanArray($array1, $array2) {
-    echo "<ul>";
-    for ($i = 0; $i < count($array1); $i++) {
-        echo "<li>Nama: " . $array1[$i] . ", Matkul Kesukaan: " . $array2[$i] . "</li>";
+function cari_nilai_terbesar($array) {
+    if (empty($array)) {
+        return NULL; // Mengembalikan NULL jika array kosong
     }
-    echo "</ul>";
+    $nilai_terbesar = $array[0]; // Menginisialisasi nilai terbesar dengan elemen pertama dari array
+    foreach ($array as $nilai) {
+        if ($nilai > $nilai_terbesar) {
+            $nilai_terbesar = $nilai;
+        }
+    }
+    return $nilai_terbesar;
 }
 
-// Memanggil fungsi untuk menampilkan array pertama dan kedua
-echo "<h3>Data Mahasiswa:</h3>";
-tampilkanArray($nama [0], $matkul[0][1]);
-tampilkanArray($nama [1], $matkul[1][2]);
-tampilkanArray($nama [2], $matkul[2][3]);
-tampilkanArray($nama [3], $matkul [3][4]);
+// Fungsi utama
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Mendapatkan nilai-nilai integer dari input
+    $input = $_POST["input"];
+    // Mengubah input menjadi array integer
+    $array_nilai = array_map('intval', explode(" ", $input));
+    
+    // Panggil fungsi untuk mencari nilai terbesar
+    $terbesar = cari_nilai_terbesar($array_nilai);
+    
+    if ($terbesar !== NULL) {
+        echo "Nilai terbesar dalam array adalah: " . $terbesar;
+    } else {
+        echo "Array kosong";
+    }
+}
 ?>
-0
--- loopingCPP_Kelompok7/Kelompok 7/main.cpp
+++ loopingCPP_Kelompok7/Kelompok 7/main.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int cari_nilai_terbesar(vector<int> array) {
     if (array.empty()) {
-        return INT_MIN;
+        return -1; // Mengembalikan -1 jika array kosong
     }
-    int nilai_terbesar = array[0];
-    for (int i = 1; i < array.size(); ++i) {
-        if (array[i] > nilai_terbesar) {
-            nilai_terbesar = array[i];
+    int nilai_terbesar = array[0]; // Menginisialisasi nilai terbesar dengan elemen pertama dari array
+    for (int nilai : array) {
+        if (nilai > nilai_terbesar) {
+            nilai_terbesar = nilai;
         }
     }
     return nilai_terbesar;
 }
 
 int main() {
-
+    // Masukkan nilai-nilai integer ke dalam vector
     vector<int> array_nilai;
     int nilai;
     cout << "Masukkan nilai-nilai integer, pisahkan dengan spasi: ";
-    while (cin >> nilai) {
+    string input;
+    getline(cin, input);
+    istringstream stream(input);
+    while (stream >> nilai) {
         array_nilai.push_back(nilai);
     }
 
-
+    // Panggil fungsi untuk mencari nilai terbesar
     int terbesar = cari_nilai_terbesar(array_nilai);
 
-    if (terbesar != INT_MIN) {
+    if (terbesar != -1) {
         cout << "Nilai terbesar dalam array adalah: " << terbesar << endl;
     } else {
         cout << "Array kosong" << endl;
